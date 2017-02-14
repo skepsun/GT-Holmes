f2e17f5e1fe3d9c299af418a403bed6e986a2037
meowoodie <woodielove@163.com>
2017-02-14 17:20:46 -0500

    remove wrong folder

--- conf/script_conf.sh
+++ conf/script_conf.sh
@@ -2,7 +2,7 @@
 
 # Basic config 
 export owner_tag='woodie'
-export task_tag='woodie.burglary.gen_vectors_from_wordslist'
+export task_tag='burglary.gen_vectors_from_wordslist'
 export created_at=`date +%Y%m%d-%H%M%S`
 
 # Resource
