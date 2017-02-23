ec31de7b0062fd3ad4dace6608ea3e0dab8d9372
meowoodie <woodielove@163.com>
2017-02-23 16:57:48 -0500

    add config file

--- conf/script_conf.sh
+++ conf/script_conf.sh
@@ -2,11 +2,5 @@
 
 # Basic config 
 export owner_tag='woodie'
-export task_tag='burglary&pedrobbery.similarity'
+export task_tag='test_new_extraction'
 export created_at=`date +%Y%m%d-%H%M%S`
-
-# Resource
-export word2vec_model_path='resource/GoogleNews-vectors-negative300.bin'
-export words_category_path='tmp/woodie.burglary.gen_vectors_from_wordslist/KeyWords.json'
-export phrases_model_path='tmp/woodie.burglary&pedrobbery.corpus/phrases.model'
-# 
