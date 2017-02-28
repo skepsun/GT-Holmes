c2bd1fbc585c578bdab67260fcd4f9f02ca8e489
meowoodie <woodielove@163.com>
2017-02-28 18:12:06 -0500

    organize config into a class

--- python/lib/utils.py
+++ python/lib/utils.py
@@ -1,22 +1,23 @@
-# Global Variables
-INI_PATH = 'conf/python.ini'
-
-### Configuration module
-
-# Init Config Object
 import ConfigParser
-Config = ConfigParser.ConfigParser()
-Config.read(INI_PATH)
 
-def ConfigSectionMap(section):
-    _dict = {}
-    options = Config.options(section)
-    for option in options:
-        try:
-            _dict[option] = Config.get(section, option)
-            if _dict[option] == -1:
-                DebugPrint("skip: %s" % option)
-        except:
-            print("exception on %s!" % option)
-            _dict[option] = None
-    return _dict
+# Global Variables
+
+class Config:
+    
+    def __init__(self, ini_path):
+        self.ini_path = ini_path
+        self.conf = ConfigParser.ConfigParser()
+        self.conf.read(INI_PATH)
+
+    def config_section_map(self, section):
+        _dict = {}
+        options = self.conf.options(section)
+        for option in options:
+            try:
+                _dict[option] = self.conf.get(section, option)
+                if _dict[option] == -1:
+                    DebugPrint("skip: %s" % option)
+            except:
+                print("exception on %s!" % option)
+                _dict[option] = None
+        return _dict
