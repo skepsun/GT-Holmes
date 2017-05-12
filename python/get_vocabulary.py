2170076c94e8e7ffaa5f82681d5d85f6a7b108dd
meowoodie <woodielove@163.com>
2017-05-12 12:56:57 +0800

    remove tesst

--- python/get_vocabulary.py
+++ python/get_vocabulary.py
@@ -1,4 +1,4 @@
-from gensim import corpora
+from gensim import corpora, models
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
 from collections import defaultdict
@@ -40,6 +40,7 @@ class Documents(object):
 
 
 if __name__ == '__main__':
+	
 	# Basic configuration
 	# ---------------------------------------------------------------
 
@@ -53,13 +54,13 @@ if __name__ == '__main__':
 	# Create a new dictionary from raw text file
 	# ---------------------------------------------------------------
 
-	# print >> sys.stderr, '[%s] Init document object from local file %s.' % (arrow.now(), raw_text_path)
-	# docs = Documents(raw_text_path)
+	print >> sys.stderr, '[%s] Init document object from local file %s.' % (arrow.now(), raw_text_path)
+	docs = Documents(raw_text_path)
 
-	# print >> sys.stderr, '[%s] Creating dictionary.' % arrow.now()
-	# dictionary = corpora.Dictionary([ doc for doc in docs ])
-	# print >> sys.stderr, '[%s] Saving dictionary at %s.' % (arrow.now(), universe_dict_path)
-	# dictionary.save(universe_dict_path)
+	print >> sys.stderr, '[%s] Creating dictionary.' % arrow.now()
+	dictionary = corpora.Dictionary([ doc for doc in docs ])
+	print >> sys.stderr, '[%s] Saving dictionary at %s.' % (arrow.now(), universe_dict_path)
+	dictionary.save(universe_dict_path)
 
 
 
@@ -74,11 +75,7 @@ if __name__ == '__main__':
 	print >> sys.stderr, '[%s] Removing stopwords, words that appear only once, and other non-words' % arrow.now()
 	non_ids  = [ tokenid for token, tokenid in iteritems(dictionary.token2id) if not re.match("^[A-Za-z_]*$", token) ]
 	once_ids = [ tokenid for tokenid, docfreq in iteritems(dictionary.dfs) if docfreq == 1 ]
-	stop_ids = [
-		dictionary.token2id[stopword] 
-		for stopword in stopwords.words('english')
-		if stopword in dictionary.token2id
-	]
+	stop_ids = [ dictionary.token2id[stopword] for stopword in stopwords.words('english') if stopword in dictionary.token2id ]
 	dictionary.filter_tokens(stop_ids + once_ids + non_ids)
 	# remove gaps in id sequence after words that were removed
 	dictionary.compactify()
@@ -98,25 +95,6 @@ if __name__ == '__main__':
 
 	print >> sys.stderr, '[%s] Saving corpus at %s.' % (arrow.now(), mm_corpus_path)
 	corpora.MmCorpus.serialize(mm_corpus_path, corpus)
-
-
-
-	# Test Corpus
-	# ---------------------------------------------------------------
-
-	# print >> sys.stderr, '[%s] Loading existed dictionary' % arrow.now()
-	# dictionary = corpora.Dictionary()
-	# dictionary = dictionary.load(pruned_dict_path)
-	# print dictionary
-
-	# print >> sys.stderr, '[%s] Loading existed corpus' % arrow.now()
-	# corpus = corpora.MmCorpus(mm_corpus_path)
-	# print corpus
-	# for doc in corpus:
-	# 	for termid, count in doc:
-	# 		print dictionary[termid],
-	# 	break
-
 
 
 
