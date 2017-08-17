0b8d5f012dc349afcb7030351df4fb2c878a78be
Shixiang Zhu <woodie@lawn-128-61-50-134.lawn.gatech.edu>
2017-08-16 20:57:52 -0400

    finish interface DBConnecter

--- python/service/dao.py
+++ python/service/dao.py
@@ -2,15 +2,25 @@
 # -*- coding: utf-8 -*-
 
 """
-
+This script contains an abstract interface for connecting various kinds of database via standard
+restful API and the data models that web service needs. Generally speaking, the data models that 
+you implement in web service inherit from interface 'DBConnecter' for getting access to the 
+database. 
 """
 
+import json
 import requests
 
 class DBConnecter():
 	"""
-	Interface (abstract class) for Connecting database via standard restful API. 
+	A simple interface (abstract class) for connecting database via secured and standard 
+	restful API. The data accessing API was provided by Loopback Database Wrapper, A simple
+	loopback server. You can define your own result parser by overriding static method 
+	"parse". 
 
+	Parameters:
+	- url:   Database restful API base url
+	- token: Access token (required by the wrapper of database for safty)
 	"""
 
 	def __init__(self, url, token):
@@ -18,28 +28,100 @@ class DBConnecter():
 			# TODO: If token was not set, it would request a new token from the server.
 			# self.get_access_token()
 			return 
-		self.url    = url
-		self.token  = token
-		self.header = { "Accept": "application/json", "Content-Type": "application/json" }
+		self.url     = url
+		self.token   = token
+		self.headers = { "Accept": "application/json", "Content-Type": "application/json" }
 
 	def __getitem__(self, id):
-		r = requests.get(url=self.url + "/%s" % id, headers=self.header, 
+		"""
+		Rewrite built-in function __getitem__
+
+		Retrieve one data item by its primary key, and return a python dict. The DAO (Data 
+		Access Operation) was done by secured and standard Restful API which are provided 
+		by Loopback Database Wrapper. 
+		"""
+
+		r = requests.get(url="%s/%s" % (self.url, id), headers=self.headers, 
 			params={ "access_token": self.token }, verify=False)
-		print r.status_code
-		# Return result if success
-		if r.status_code / 10 == 20: # status_code starts with 20*
-			return r.json()
-		# Invalid token
-		elif r.status_code == 401:
-			raise Exception("Invalid token")
+		# Return result if success (status == 2XX)
+		if r.status_code / 10 == 20:
+			return self.parse(r.json())
+		# Invalid request
 		else:
-			raise Exception("Failed to get value.")
+			return r.json()
+
+	def get(self, key, vals):
+		"""
+		Get Multiple Items
+
+		Retrieve multiple data items by indicated key and its values. key is supposed to be
+		a string, and vals is a list of values. Basically, this method is implemented by 
+		sending a restful request with a filter parameter which contains a where filter. It
+		would be a better way to fetch multiple data items rather than requesting by id for 
+		many times. Noted that the response would return a list of intact data item, and 
+		"parse" function would be applied here for each of the item by default. 
+		"""
+
+		key_vals = [ {key: val} for val in vals ]
+		filter   = { "where": { "or": key_vals } }
+		params   = { "access_token": self.token, "filter": json.dumps(filter) }
+		r = requests.get(url=self.url, headers=self.headers, params=params, verify=False)
+		# Return result if success (status == 2XX)
+		if r.status_code / 10 == 20:
+			return [ self.parse(item) for item in r.json() ]
+		# Invalid request
+		else:
+			return r.json()
 
 	@staticmethod
-	def get_access_token(email, password):
-		pass
+	def parse(result):
+		"""
+		Parse
+
+		The static method would be invoked everytimes when __getitem__ was excuted (getting
+		resource by id). it would help to parse the response and return to caller if this 
+		method was overrided. In the abstract class DBConnecter, this method would do nothing
+		but return response directly. 
+		"""
+
+		return result
+
+
+
+class BasicInfo(DBConnecter):
+	"""
+	Basic Info 
+
+	This class is a data model for the table "basic_info" of database "APD_Data" in local MySQL.
+	Basically, this class persists a unique url (/api/basic_infos) for acquiring information
+	from table "basic_info", and it also overrides the static method "parse" to extract specific
+	fields of data from the raw response.
+	"""
+
+	def __init__(self, token):
+		self.url   = "https://139.162.173.91:3000/api/basic_infos"
+		self.token = token
+		DBConnecter.__init__(self, self.url, self.token)
+
+	@staticmethod
+	def parse(result):
+		"""
+		Overriding of "parse" in DBConnecter
+
+		
+		"""
+		avg_lat  = result["avg_lat"]
+		avg_long = result["avg_long"]
+		city     = result["city"]
+		date     = result["incident_date"]
+		priority = result["priority"]
+
+		return result
 
 if __name__ == "__main__":
 
+	# dao = DBConnecter("https://139.162.173.91:3000/api/basic_infos", "gatech1234!")
+	# print dao["161881787"]
+
 	dao = DBConnecter("https://139.162.173.91:3000/api/basic_infos", "gatech1234!")
-	print dao["161881787"]
+	print dao.get("incident_num", ["130010040", "130010041", "130010039"])
